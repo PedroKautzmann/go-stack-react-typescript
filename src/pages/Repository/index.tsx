67d36fa4dec375378bf72ff1d394cea7a84b1298
Pedro Kautzmann <pedrokautzmann1@gmail.com>
2020-07-30 19:37:23 -0300

    feature: Dashboard page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import { Header, RepositoryInfo } from './styles';
+import githubLogo from '../../assets/github_logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={githubLogo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/69631?s=200&v=4"
+            alt="Facebook"
+          />
+          <div>
+            <strong>facebook/react</strong>
+            <p>descrição</p>
+          </div>
+        </header>
+        <ul />
+      </RepositoryInfo>
+    </>
+  );
 };
 
 export default Repository;
